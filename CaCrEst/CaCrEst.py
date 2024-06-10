20b2f1ad145095630cd5ee138e380e5a6eb6b944
CassianoMarques <137843836+Yugaren@users.noreply.github.com>
2024-06-10 14:54:01 -0300

    Add files via upload

--- CaCrEst/CaCrEst.py
+++ CaCrEst/CaCrEst.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 def adjust_column_width(ws):
     for col in ws.columns:
         max_length = 0
-        column = col[0].column_letter  # Get the column name
+        column = col[0].column_letter  # Nome colunas
         for cell in col:
             try:
                 if len(str(cell.value)) > max_length:
@@ -28,25 +28,25 @@ def add_borders(ws):
             cell.border = Border(top=thin, left=thin, right=thin, bottom=thin)
 
 def process_excel(file_path):
-    # Ler o arquivo Excel
+    # Lê o arquivo Excel
     df = pd.read_excel(file_path)
 
-    # Aplicar fórmula para a Coluna A (valor do arquivo de entrada + 0)
+    # Coluna A 
     df['Circunferência'] = df.iloc[:, 0] + 0
 
-    # Calcular a Coluna B
+    # Coluna B
     df['Diâmetro'] = (df['Circunferência'] / 3.1416).round().astype(int)
 
-    # Calcular a Coluna C
+    # Coluna C
     df['Partes perenes acima e abaixo do solo mais folhas'] = (exp(-6.0032) * df['Diâmetro']**3.7578).round(1)
 
-    # Calcular a Coluna D
+    # Coluna D
     df['Biomassa C (kg)'] = (df['Partes perenes acima e abaixo do solo mais folhas'] / 2).round(1)
 
-    # Calcular a Coluna F
+    # Coluna F
     df['CO2eq'] = (0.004198 * df['Diâmetro']**3.779016).round(2)
 
-    # Adicionar linhas de média
+    # Linhas de média
     mean_row = {
         'Circunferência': 'Média',
         'Diâmetro': df['Diâmetro'].mean().round(),
@@ -58,20 +58,20 @@ def process_excel(file_path):
     mean_df = pd.DataFrame([mean_row])
     df = pd.concat([df, mean_df], ignore_index=True)
 
-    # Selecionar apenas as colunas desejadas para o arquivo de saída
+    # Seleciona apenas as colunas desejadas para o arquivo de saída
     df_output = df[['Circunferência', 'Diâmetro', 'Partes perenes acima e abaixo do solo mais folhas', 'Biomassa C (kg)', 'CO2eq']]
 
-    # Salvar o arquivo Excel processado
+    # Salva o arquivo Excel 
     output_file = file_path.replace('.xlsx', '_Calculado.xlsx')
     df_output.to_excel(output_file, index=False)
 
-    # Ajustar a largura das células e adicionar bordas
+    # Largura das células e adiciona bordas
     wb = load_workbook(output_file)
     ws = wb.active
     adjust_column_width(ws)
     add_borders(ws)
 
-    # Adicionar cor verde intermediária à linha do cabeçalho (linha 1) e à linha de média (última linha)
+    # Cor do cabeçalho (linha 1) e linha de média (última linha)
     medium_green_fill = PatternFill(start_color="00C000", end_color="00C000", fill_type="solid")
     header_row = ws[1]
     mean_row = ws[ws.max_row]
@@ -82,8 +82,8 @@ def process_excel(file_path):
 
     wb.save(output_file)
 
-    # Gerar gráfico
-    num_plants = len(df) - 1  # Excluindo a linha de média
+    # Gera gráfico
+    num_plants = len(df) - 1  # Exclui linha de média
     diameter_classes = ['19 a 22', '22 a 25', '25 a 28', '28 a 31', '31 a 34', '34 a 37', '37 a 40']
     diameter_counts = [((df['Diâmetro'] >= 19) & (df['Diâmetro'] <= 22)).sum(),
                        ((df['Diâmetro'] > 22) & (df['Diâmetro'] <= 25)).sum(),
@@ -98,11 +98,11 @@ def process_excel(file_path):
     plt.ylabel('Nº de Plantas')
     plt.title('Distribuição do Diâmetro das Plantas')
     plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.3)  # Ajuste das margens
-    plt.savefig(file_path.replace('.xlsx', '_Graph.png'))  # Salvar o gráfico como uma imagem
-    plt.close()  # Fechar a figura após salvar
+    plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.3)  # Ajusta margens
+    plt.savefig(file_path.replace('.xlsx', '_Graph.png'))  # Salva o gráfico como uma imagem
+    plt.close()  # Fecha a figura após salvar
 
-    # Mostrar mensagem de sucesso na interface
+    # Mensagem de sucesso
     success_label.config(text=f"Arquivo processado salvo como {output_file}")
 
 def select_file():
@@ -110,7 +110,7 @@ def select_file():
     if file_path:
         process_excel(file_path)
 
-# Configuração da interface gráfica
+# Interface gráfica
 root = tk.Tk()
 root.title("CaCrEst")
 
@@ -123,5 +123,5 @@ select_button.pack()
 success_label = tk.Label(frame, text="")
 success_label.pack()
 
-root.geometry("580x200")  # Aumenta o tamanho do painel
+root.geometry("580x200")  # Tamanho do painel
 root.mainloop()
